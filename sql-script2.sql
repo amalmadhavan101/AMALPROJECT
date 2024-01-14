5c51704503cf2faf432c3ec0ecdd068c1a7d9b93
amalmadhavan101 <133011892+amalmadhavan101@users.noreply.github.com>
2024-01-14 13:09:19 -0500

    Create sql-script2.sql

---
+++ sql-script2.sql
@@ -0,0 +1,18 @@
+-- Create a database
+CREATE DATABASE IF NOT EXISTS amal124;
+
+-- Use the created database
+USE amal124;
+
+-- Create a table
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
+
+-- Insert some data into the table
+INSERT INTO users (id, username, email) VALUES
+(1, 'ama', 'john@example.com'),
+(2, 'kic', 'jane@example.com');
+select * from users;
