c1d3ce86168d14c72d75efa20fa2836bed7355f9
amalmadhavan101 <133011892+amalmadhavan101@users.noreply.github.com>
2024-01-23 14:17:49 -0500

    Update sql_script.sql

--- sql_script.sql
+++ sql_script.sql
@@ -5,14 +5,14 @@
 USE amal123;
 
 -- Create a table
---CREATE TABLE IF NOT EXISTS userss (
+-- CREATE TABLE IF NOT EXISTS userss (
  --   id INT PRIMARY KEY,
    -- username VARCHAR(255) NOT NULL,
     -- email VARCHAR(255) NOT NULL
---);
+-- );
 
 -- Insert some data into the table
---INSERT INTO userss (id, username, email) VALUES
---(1, 'john_doe', 'john@example.com'),
---(2, 'jane_doe', 'jane@example.com');
+-- INSERT INTO userss (id, username, email) VALUES
+-- (1, 'john_doe', 'john@example.com'),
+-- (2, 'jane_doe', 'jane@example.com');
 select * from userss;
